89f8b95bc345ab6944ee9289c2459a10bb454571
kalbetre-alx <kalbetre@icloud.com>
2022-06-02 05:32:08 -0700

    add c program to print texting using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the specified text
+ *
+ * Description: prints Programming th proper gramer, but the outcome
+ * is a piece of art,
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
