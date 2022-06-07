fa11236ecfd2d64ac8c822d20d1cea1b257c694e
kalbetre-alx <kalbetre@icloud.com>
2022-06-07 15:05:08 -0700

    fix large number sum

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,10 +8,12 @@
  */
 int main(void)
 {
-	long n1 = 1, n2 = 2, n3, counter = 3;
+	int counter = 3, overflow;
+	unsigned long n1 = 1, n2 = 2, n3;
+	long n1_head, n1_tail, n2_head, n2_tail, sum_head, sum_tail;
 
 	printf("%lu, %lu", n1, n2);
-	while (counter <= 98)
+	while (counter < 93)
 	{
 		n3 = n1 + n2;
 		printf(", %lu", n3);
@@ -19,6 +21,27 @@ int main(void)
 		n2 = n3;
 		counter++;
 	}
+
+	n1_head = n1 / 1000000000;
+	n1_tail = n1 % 1000000000;
+	n2_head = n2 / 1000000000;
+	n2_tail = n2 % 1000000000;
+
+	while (counter <= 98)
+	{
+		overflow = (n1_tail + n2_tail) / 1000000000;
+		sum_tail = (n1_tail + n2_tail) - (1000000000 * overflow);
+		sum_head = (n1_head + n2_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		n1_head = n2_head;
+		n1_tail = n2_tail;
+		n2_head = sum_head;
+		n2_tail = sum_tail;
+		counter++;
+	}
+
 	putchar('\n');
 	return (0);
 }
