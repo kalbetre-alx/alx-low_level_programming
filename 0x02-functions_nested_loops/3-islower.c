240c3c0a5c2afe970bc6fa019548055963e568c0
kalbetre-alx <kalbetre@icloud.com>
2022-06-07 07:04:57 -0700

    add a function to check for lowercase alpha

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a char is in lowercase
+ *
+ * Description: checks if a char is lowercase by
+ * using its ascii code
+ * Return: 1 if c is lowercase otherwise returns 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
