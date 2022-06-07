7f9c60c03d56eea86bafc85d0f249b08dbf5a6fb
kalbetre-alx <kalbetre@icloud.com>
2022-06-07 12:21:39 -0700

    add a function to add tow numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Description: add sto integers
+ * Return: sum of the numbers
+ */
+int add(int n1, int n2)
+{
+	return n1 + n2;
+}
