bdd83646097336be59793e3c65866e667e607992
kalbetre-alx <kalbetre@icloud.com>
2022-06-09 12:14:57 -0700

    add Fizz-Buzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+/**
+ * main - Fizz-Buzz Program
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, max = 100;
+
+	for (i = 1; i <= max; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		
+		if (i != max)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
