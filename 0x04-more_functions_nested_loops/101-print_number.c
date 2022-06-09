46d2c808c6078eab78d35591d143c93e71776ae2
kalbetre-alx <kalbetre@icloud.com>
2022-06-09 14:52:34 -0700

    using unsigned int to increase range of integers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,22 +9,21 @@
 
 void print_number(int n)
 {
-	int factor = 1;
+	unsigned int num, digit;
+	double factor = 1;
 
+	num = n > 0 ? n : n * -1;
 	if (n < 0)
-	{
 		_putchar('-');
-		n *= -1;
-	}
 
-	while (n / factor >= 10)
+	while (num / factor >= 10)
 		factor *= 10;
 
 	while (factor >= 1)
 	{
-		_putchar(n / factor + '0');
-		n = n - (n / factor) * factor;
+		digit = num / factor;
+		_putchar(digit + '0');
+		num = num - digit * factor;
 		factor /= 10;
 	}
-
 }
