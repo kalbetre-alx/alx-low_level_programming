5c8911901e8bab7d4211f6e6fc672b72f5d7633b
kalbetre-alx <kalbetre@icloud.com>
2022-06-09 13:45:15 -0700

    add a c program to print prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the prime factors of a number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long n = 612852475143, factor = 2;
+
+	while (n >= factor * factor)
+	{
+		if (n % factor == 0)
+		{
+			n = n / factor;
+			printf("%lu ", factor);
+		}
+		else
+		{
+			factor++;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
