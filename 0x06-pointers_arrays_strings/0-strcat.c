869a320456fcbeaae7cfe68a1e65e4e4e07cc7e5
kalbetre-alx <kalbetre@icloud.com>
2022-06-14 23:50:13 -0700

    fix betty style for strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - returns the length of a string
+ * _strcat - concatenates src string to dest string
  * @dest: pointer to the destination string
  * @src: pointer to the source string
  *
@@ -10,19 +10,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-    int dest_i = 0, src_i = 0;
-    
-    while (*(dest + dest_i) != '\0')
-        dest_i++;
-    
-    while (*(src + src_i) != '\0')
-    {
-        *(dest + dest_i) = *(src + src_i);
-        dest_i++;
-        src_i++;
-    }
-    
-    *(dest + dest_i) = '\0';
-      
-    return dest;
+	int dest_i = 0, src_i = 0;
+
+	while (*(dest + dest_i) != '\0')
+		dest_i++;
+
+	while (*(src + src_i) != '\0')
+	{
+		*(dest + dest_i) = *(src + src_i);
+		dest_i++;
+		src_i++;
+	}
+
+	*(dest + dest_i) = '\0';
+
+	return (dest);
 }
