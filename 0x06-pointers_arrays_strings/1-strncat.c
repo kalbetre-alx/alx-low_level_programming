9fd11c09b1b0a4649f2ecc5bbfb14231a6ac2368
kalbetre-alx <kalbetre@icloud.com>
2022-06-14 23:56:16 -0700

    fix betty style for strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,25 +4,26 @@
  * _strncat - copies n characters from src to dest
  * @dest: pointer to the destination string
  * @src: pointer to the source string
+ * @n: number of char to concatenate
  *
  * Return: pointer to the dest string
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_i = 0, src_i = 0;
-    
-    while (*(dest + dest_i) != '\0')
-        dest_i++;
-    
-    while (src_i < n && *(src + src_i) != '\0')
-    {
-        *(dest + dest_i) = *(src + src_i);
-        dest_i++;
-        src_i++;
-    }
-    
-    *(dest + dest_i) = '\0';
-      
-    return dest;
+	int dest_i = 0, src_i = 0;
+
+	while (*(dest + dest_i) != '\0')
+		dest_i++;
+
+	while (src_i < n && *(src + src_i) != '\0')
+	{
+		*(dest + dest_i) = *(src + src_i);
+		dest_i++;
+		src_i++;
+	}
+
+	*(dest + dest_i) = '\0';
+
+	return (dest);
 }
