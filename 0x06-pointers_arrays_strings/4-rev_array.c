aff4fb962210fa39be8c122d309b61a16c72934c
kalbetre-alx <kalbetre@icloud.com>
2022-06-15 12:56:20 -0700

    add a function to reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+/**
+ * reverse_array - reverses the content of array
+ * @a: the array
+ * @n: size of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n - i - 1);
+		*(a + n - i - 1) = temp;
+	}
+}
