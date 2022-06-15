754942c5dc53e65627c5a19d7c73f60db30e15c2
kalbetre-alx <kalbetre@icloud.com>
2022-06-15 12:31:18 -0700

    add compare function

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+/*
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Returns: 1 if s1 is greater, -1 if lessert or 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int index = 0;
+
+	while (*(s1 + index) != '\0' && *(s2 + index) != '\0')
+	{
+		if (*(s1 + index) > *(s2 + index))
+			return (1);
+		else if (*(s1 + index) < *(s2 + index))
+			return (-1);
+		index++;
+	}
+
+	if (*(s1 + index) != '\0' || *(s2 + index) != '\0')
+		return (*(s1 + index) != '\0' ? 1 : -1);
+
+	return (0);
+}
