6e9b64771f885500e721a5c1280398f555968089
Kalkidan Betre <105339878+kalbetre-alx@users.noreply.github.com>
2022-06-15 09:41:37 +0300

    add a program to concatenate only n characters

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat - copies n characters from src to dest
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ *
+ * Return: pointer to the dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+    int dest_i = 0, src_i = 0;
+    
+    while (*(dest + dest_i) != '\0')
+        dest_i++;
+    
+    while (src_i < n && *(src + src_i) != '\0')
+    {
+        *(dest + dest_i) = *(src + src_i);
+        dest_i++;
+        src_i++;
+    }
+    
+    *(dest + dest_i) = '\0';
+      
+    return dest;
+}
