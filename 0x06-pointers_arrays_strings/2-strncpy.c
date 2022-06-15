cc4709a32b5a1ece528e32e2ad9388c5873a6e17
kalbetre-alx <kalbetre@icloud.com>
2022-06-15 00:02:34 -0700

    fix betty style for strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -4,35 +4,36 @@
  * _strncpy - copies only n characters from src to dest
  * @dest: pointer to the dest string
  * @src: pointer to the src string
+ * @n: number of chars to copy
  *
  * Return: pointer to dest string
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-    int src_i = 0, dest_len = 0, stop_flag = 0;
-    
-    if (dest != NULL)
-    {
-        while (*(dest + dest_len) != '\0')
-            dest_len++;
-    }
+	int src_i = 0, dest_len = 0, stop_flag = 0;
 
-    while (src_i < n)
-    {
-        if (*(src + src_i) == '\0')
-            stop_flag = 1;
-            
-        if (!stop_flag)
-            *(dest + src_i) = *(src + src_i);
-        else
-            *(dest + src_i) = '\0';
-            
-        src_i++;
-    }
-    
-    if (src_i > dest_len)
-        *(dest + src_i) = '\0';
-      
-    return dest;
+	if (dest != NULL)
+	{
+		while (*(dest + dest_len) != '\0')
+			dest_len++;
+	}
+
+	while (src_i < n)
+	{
+		if (*(src + src_i) == '\0')
+			stop_flag = 1;
+
+		if (!stop_flag)
+			*(dest + src_i) = *(src + src_i);
+		else
+			*(dest + src_i) = '\0';
+
+		src_i++;
+	}
+
+	if (src_i > dest_len)
+		*(dest + src_i) = '\0';
+
+	return (dest);
 }
