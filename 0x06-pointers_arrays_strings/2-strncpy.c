59595984c9d84b22c90e6077bf7ab84bc0899a17
Kalkidan Betre <105339878+kalbetre-alx@users.noreply.github.com>
2022-06-15 09:43:19 +0300

    add a program that copies n chars from src to dest

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies only n characters from src to dest
+ * @dest: pointer to the dest string
+ * @src: pointer to the src string
+ *
+ * Return: pointer to dest string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int src_i = 0, dest_len = 0, stop_flag = 0;
+    
+    if (dest != NULL)
+    {
+        while (*(dest + dest_len) != '\0')
+            dest_len++;
+    }
+
+    while (src_i < n)
+    {
+        if (*(src + src_i) == '\0')
+            stop_flag = 1;
+            
+        if (!stop_flag)
+            *(dest + src_i) = *(src + src_i);
+        else
+            *(dest + src_i) = '\0';
+            
+        src_i++;
+    }
+    
+    if (src_i > dest_len)
+        *(dest + src_i) = '\0';
+      
+    return dest;
+}
