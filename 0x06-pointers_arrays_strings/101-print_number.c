0f07810a29ffe191ad29fec47ac53c1dbf0fd26f
kalbetre-alx <kalbetre@icloud.com>
2022-06-15 15:20:30 -0700

    add function to print numbers

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer number
+ * @n: the number
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int pos_n;
+	short digit, tens = 1, count, i = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		pos_n = n < 0 ? -1 * n : n;
+		if (n < 0)
+			_putchar('-');
+
+		while ((pos_n / tens > 0))
+			tens *= 10;
+		tens /= 10;
+
+		while (tens >= 1)
+		{
+			digit = pos_n / tens;
+			pos_n = pos_n - digit * tens;
+			tens /= 10;
+			_putchar(digit + '0');
+		}
+	}
+}
