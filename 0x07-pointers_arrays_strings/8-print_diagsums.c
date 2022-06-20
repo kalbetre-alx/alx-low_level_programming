53ca5f3a8cb503c7aa66b689cc20357cdff76354
kalbetre-alx <kalbetre@icloud.com>
2022-06-20 15:48:20 -0700

    add print diagsum function

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints diagonal sums of a matrix
+ * @a: the matrix
+ * @size: size of the matrix
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	unsigned int i, j, diag_1 = 0, diag_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+				diag_1 += a[i * size + j];
+			if (j == size - i - 1)
+				diag_2 += a[i * size + j];
+		}
+	}
+
+	printf("%d, %d\n", diag_1, diag_2);
+}
