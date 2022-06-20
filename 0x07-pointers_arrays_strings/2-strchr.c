8aab44d8df2a828eb66fb1fd0d60c506274315ee
kalbetre-alx <kalbetre@icloud.com>
2022-06-20 13:51:58 -0700

    add strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,28 @@
+/**
+ * _strchr - locates a character in a string
+ * @s: the pointer to the string
+ * @c: the character to search
+ *
+ * Return: returns a pointer first occurrence of c
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+	short is_found = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			is_found = 1;
+			break;
+		}
+		i++;
+	}
+
+	if (is_found)
+		return (s + i);
+	else
+		return (NULL);
+}
