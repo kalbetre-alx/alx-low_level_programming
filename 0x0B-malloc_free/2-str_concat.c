b697bd366c43128f6dc713af9a0379a01bd3555b
kalbetre-alx <kalbetre@icloud.com>
2022-06-26 12:16:36 -0700

    fix NULL strings concatination

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,14 +28,14 @@ char *str_concat(char *s1, char *s2)
 	if (str_comb == NULL)
 		return (NULL);
 
-	while (s1[i] != '\0')
+	while (s1 != NULL && s1[i] != '\0')
 	{
 		str_comb[i] = s1[i];
 		i++;
 	}
 
 	i = 0;
-	while (s2[i] != '\0')
+	while (s2 != NULL && s2[i] != '\0')
 	{
 		str_comb[i + s1_len] = s2[i];
 		i++;
