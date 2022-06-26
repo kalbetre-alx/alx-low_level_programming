b841f91cd60bfecf5e4d8b5ea71426bdf9568e33
kalbetre-alx <kalbetre@icloud.com>
2022-06-26 12:11:54 -0700

    add a function to join two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+
+/**
+ *str_concat - concatenates two string
+ * @s1: the first string
+ * @s2: the second string
+ *
+ * Return: pointer to the joined string or NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str_comb;
+	int i = 0, s1_len = 0, s2_len = 0;
+
+	if (s1 != NULL)
+	{
+		while (s1[s1_len] != '\0')
+			s1_len++;
+	}
+
+	if (s2 != NULL)
+	{
+		while (s2[s2_len] != '\0')
+			s2_len++;
+	}
+
+	str_comb = (char *)malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (str_comb == NULL)
+		return (NULL);
+
+	while (s1[i] != '\0')
+	{
+		str_comb[i] = s1[i];
+		i++;
+	}
+
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		str_comb[i + s1_len] = s2[i];
+		i++;
+	}
+
+	str_comb[s1_len + s2_len] = '\0';
+	return (str_comb);
+}
