a52f3da06424baf80b1025730478411f8ca60302
kalbetre-alx <kalbetre@icloud.com>
2022-06-27 07:34:32 -0700

    fix betty style for strtwo

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,25 +9,25 @@
  */
 char *next_word(char *str, int *word_size)
 {
-    int i = 0;
+	int i = 0;
 
-    while (*(str + i) != '\0')
-    {
-        while (*(str + i) == ' ' && *(str + i) != '\0')
-                i++;
+	while (*(str + i) != '\0')
+	{
+		while (*(str + i) == ' ' && *(str + i) != '\0')
+			i++;
 
-        *word_size = 0;
-        while (*(str + i) != ' ' && *(str + i) != '\0')
-        {
-            (*word_size)++;
-            i++;
-        }
-        
-        if (*word_size > 0)
-            return str + (i - *word_size);
-    }
-    
-    return NULL;
+		*word_size = 0;
+		while (*(str + i) != ' ' && *(str + i) != '\0')
+		{
+			(*word_size)++;
+			i++;
+		}
+
+		if (*word_size > 0)
+			return (str + (i - *word_size));
+	}
+
+	return (NULL);
 }
 
 /**
@@ -38,16 +38,16 @@ char *next_word(char *str, int *word_size)
  */
 int word_count(char *str)
 {
-    int count = 0, i = 0, word_size = 0;
-    char *word = next_word(str, &word_size);
-    
-    while (word != NULL)
-    {
-        count++;
-        word = next_word(word + word_size, &word_size);
-    }
+	int count = 0, i = 0, word_size = 0;
+	char *word = next_word(str, &word_size);
 
-    return count;
+	while (word != NULL)
+	{
+		count++;
+		word = next_word(word + word_size, &word_size);
+	}
+
+	return (count);
 }
 
 /**
@@ -58,36 +58,35 @@ int word_count(char *str)
  */
 char **strtow(char *str)
 {
-    int i = 0, j = 0, word_size = 0, num_words = 0;
-    char **words;
-    char *word;
-    
-    if (str == NULL || str == "")
-        return (NULL);
-    
-    num_words = word_count(str);
-    words = malloc(sizeof(char *) * (num_words + 1));
-    if (words == NULL)
-        return (NULL);
-    
-    word = next_word(str, &word_size);
-    while (word != NULL)
-    {
-        words[i] = (char *)malloc(sizeof(char) * (word_size + 1));
-        if (words[i] == NULL)
-            return (NULL);
-        
-        j = 0;
-        while (j < word_size)
-        {
-            words[i][j] = word[j];
-            j++;
-        }
-        words[i][j] = '\0';
-        i++;
-        
-        word = next_word(word + word_size, &word_size);
-    }
-    words[i] = NULL;
-    return (words);
+	int i = 0, j = 0, word_size = 0, num_words = 0;
+	char **words;
+	char *word;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	num_words = word_count(str);
+	words = malloc(sizeof(char *) * (num_words + 1));
+	if (words == NULL)
+		return (NULL);
+
+	word = next_word(str, &word_size);
+	while (word != NULL)
+	{
+		words[i] = (char *)malloc(sizeof(char) * (word_size + 1));
+		if (words[i] == NULL)
+			return (NULL);
+		j = 0;
+		while (j < word_size)
+		{
+			words[i][j] = word[j];
+			j++;
+		}
+		words[i][j] = '\0';
+		i++;
+
+		word = next_word(word + word_size, &word_size);
+	}
+	words[i] = NULL;
+	return (words);
 }
