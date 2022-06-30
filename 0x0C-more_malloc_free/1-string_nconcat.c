783e9f672f384ba1fd3de5a479e1f2e9c2b9a797
kalbetre-alx <kalbetre@icloud.com>
2022-06-30 09:36:48 -0700

    add string concatination function

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+/**
+ * string_nconcat - combines two strings given the limit for the second
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the number of chars from s2 to copy
+ *
+ * Return: pointer to the concatenated string or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int s1_len, s2_len, i = 0;
+	char *combo;
+
+	s1_len = s1 == NULL ? 0 : strlen(s1);
+	s2_len = s2 == NULL ? 0 : strlen(s2);
+	n = n > s2_len ? s2_len : n;
+
+	combo = malloc(s1_len + n + 1);
+	if (combo == NULL)
+		return (NULL);
+
+	while (*(s1 + i) != '\0')
+	{
+		*(combo + i) = *(s1 + i);
+		i++;
+	}
+
+	i = 0;
+	while (i < n)
+	{
+		*(combo + s1_len + i) = *(s2 + i);
+		i++;
+	}
+
+	*(combo + s1_len + n) = '\0';
+	return (combo);
+}
