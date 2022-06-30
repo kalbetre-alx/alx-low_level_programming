30627c8f64cf437aaaa574431aa3acea4f5acc93
Kalkidan Betre <105339878+kalbetre-alx@users.noreply.github.com>
2022-07-01 02:54:30 +0300

    update 101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,88 +1,77 @@
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
-#include "main.h"
 
 /**
- * isValidNumber - checks if a string is a valid number
- * @num: the given number
+ * _isdigit - checks if character is digit
+ * @c: the character to check
  *
- * Return: 1 if it is valid or 0
+ * Return: 1 if digit, 0 otherwise
  */
-int isValidNumber(char *num)
+int _isdigit(int c)
 {
-	while (*num)
-	{
-		if (*num < '0' || *num > '9')
-			return (0);
-		num++;
-	}
-	return (1);
+	return (c >= '0' && c <= '9');
 }
 
 /**
- * _puts - prints a string followed by a new line
- * @str: the string
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: Nothing
+ * Return: integer length of string
  */
-void _puts(char *str)
+int _strlen(char *s)
 {
-	if (isValidNumber(str))
-	{
-		while (*str == '0')
-			str++;
-	}
-	while (*str)
-		_putchar(*str++);
-	_putchar('\n');
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
 }
 
 /**
- * mul - multiplies two large numbers
- * @s1: first number
- * @s2: second number
+ * big_multiply - multiply two big number strings
+ * @s1: the first big number string
+ * @s2: the second big number string
  *
- * Return: s1 times s2
+ * Return: the product big number string
  */
-char *mul(char *s1, char *s2)
+char *big_multiply(char *s1, char *s2)
 {
 	char *r;
 	int l1, l2, a, b, c, x;
 
-	l1 = strlen(s1);
-	l2 = strlen(s2);
+	l1 = _strlen(s1);
+	l2 = _strlen(s2);
 	r = malloc(a = x = l1 + l2);
-
 	if (!r)
-		_puts("Error"), exit(98);
-
+		printf("Error\n"), exit(98);
 	while (a--)
 		r[a] = 0;
 
 	for (l1--; l1 >= 0; l1--)
 	{
-		if (!isdigit(s1[l1]))
+		if (!_isdigit(s1[l1]))
 		{
 			free(r);
-			_puts("Error"), exit(98);
+			printf("Error\n"), exit(98);
 		}
 		a = s1[l1] - '0';
 		c = 0;
 
-		for (l2 = strlen(s2) - 1; l2 >= 0; l2--)
+		for (l2 = _strlen(s2) - 1; l2 >= 0; l2--)
 		{
-			if (!isdigit(s2[l2]))
+			if (!_isdigit(s2[l2]))
 			{
 				free(r);
-				_puts("Error"), exit(98);
+				printf("Error\n"), exit(98);
 			}
 			b = s2[l2] - '0';
 
 			c += r[l1 + l2 + 1] + (a * b);
 			r[l1 + l2 + 1] = c % 10;
-			
-			c /= 10;		
+
+			c /= 10;
 		}
 		if (c)
 			r[l1 + l2 + 1] += c;
@@ -90,23 +79,24 @@ char *mul(char *s1, char *s2)
 	return (r);
 }
 
+
 /**
- * main - multiplies two numbers
- * @argc: number of arguments passed
- * @argv: array of values passed to the the program
+ * main - multiply two big number strings
+ * @argc: the number of arguments
+ * @argv: the argument vector
  *
- * Return: the multiplication of the numbers
+ * Return: Always 0 on success.
  */
 int main(int argc, char **argv)
 {
 	char *r;
 	int a, c, x;
-	
-	if (argc != 3)
-		_puts("Error"), exit(98);
 
-	x = strlen(argv[1]) + strlen(argv[2]);
-	r = mul(argv[1], argv[2]);
+	if (argc != 3)
+		printf("Error\n"), exit(98);
+
+	x = _strlen(argv[1]) + _strlen(argv[2]);
+	r = big_multiply(argv[1], argv[2]);
 	c = 0;
 	a = 0;
 	while (c < x)
