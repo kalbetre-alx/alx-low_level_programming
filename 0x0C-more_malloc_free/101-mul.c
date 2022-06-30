af9930127a715abdd19788c75359a73103ce14ee
kalbetre-alx <kalbetre@icloud.com>
2022-06-30 16:50:46 -0700

    revise mul algo

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,7 +13,7 @@ int isValidNumber(char *num)
 {
 	while (*num)
 	{
-		if (!isdigit(*num))
+		if (*num < '0' || *num > '9')
 			return (0);
 		num++;
 	}
@@ -40,47 +40,54 @@ void _puts(char *str)
 
 /**
  * mul - multiplies two large numbers
- * @n1: first number
- * @n2: second number
+ * @s1: first number
+ * @s2: second number
  *
- * Return: n1 times n2
+ * Return: s1 times s2
  */
-char *mul(char *n1, char *n2)
+char *mul(char *s1, char *s2)
 {
-	char *product;
-	char d;
-	int n1_len, n2_len, pr_len, carry, sum, j = 0;
+	char *r;
+	int l1, l2, a, b, c, x;
 
-	n1_len = strlen(n1);
-	n2_len = strlen(n2);
-	pr_len = n1_len + n2_len;
-	product = malloc(pr_len + 1);
-	if (product == NULL)
-		return (NULL);
-	while (j < pr_len)
+	l1 = strlen(s1);
+	l2 = strlen(s2);
+	r = malloc(a = x = l1 + l2);
+
+	if (!r)
+		_puts("Error"), exit(98);
+
+	while (a--)
+		r[a] = 0;
+
+	for (l1--; l1 >= 0; l1--)
 	{
-		*(product + j) = '0';
-		j++;
-	}
-	n2_len--;
-	n1_len--;
-	while (n2_len >= 0)
-	{
-		carry = 0;
-		j = n1_len;
-		while (j >= 0)
+		if (!isdigit(s1[l1]))
 		{
-			d = product[j + n2_len + 1] - 48;
-			sum = carry + d + (n1[j] - 48) * (n2[n2_len] - 48);
-			carry = sum / 10;
-			product[j + n2_len + 1] = sum % 10 + '0';
-			j--;
+			free(r);
+			_puts("Error"), exit(98);
 		}
-		product[n2_len] = carry + '0';
-		n2_len--;
+		a = s1[l1] - '0';
+		c = 0;
+
+		for (l2 = strlen(s2) - 1; l2 >= 0; l2--)
+		{
+			if (!isdigit(s2[l2]))
+			{
+				free(r);
+				_puts("Error"), exit(98);
+			}
+			b = s2[l2] - '0';
+
+			c += r[l1 + l2 + 1] + (a * b);
+			r[l1 + l2 + 1] = c % 10;
+			
+			c /= 10;		
+		}
+		if (c)
+			r[l1 + l2 + 1] += c;
 	}
-	product[pr_len] = '\0';
-	return (product);
+	return (r);
 }
 
 /**
@@ -92,26 +99,27 @@ char *mul(char *n1, char *n2)
  */
 int main(int argc, char **argv)
 {
-	char *result;
+	char *r;
+	int a, c, x;
+	
+	if (argc != 3)
+		_puts("Error"), exit(98);
 
-	if (argc != 3 || !isValidNumber(argv[1]) || !isValidNumber(argv[2]))
+	x = strlen(argv[1]) + strlen(argv[2]);
+	r = mul(argv[1], argv[2]);
+	c = 0;
+	a = 0;
+	while (c < x)
 	{
-		_puts("Error");
-		exit(98);
+		if (r[c])
+			a = 1;
+		if (a)
+			_putchar(r[c] + '0');
+		c++;
 	}
-	else
-	{
-		result = mul(argv[1], argv[2]);
-		if (result == NULL)
-		{
-			_puts("Error");
-			exit(98);
-		}
-		else
-		{
-			_puts(result);
-			free(result);
-		}
-	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(r);
 	return (0);
 }
