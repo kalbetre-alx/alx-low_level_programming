1eb63ebaa929b04a8fc9ee42c675c404e13becca
kalbetre-alx <kalbetre@icloud.com>
2022-06-30 15:23:09 -0700

    suppor mul of negative numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,6 +11,9 @@
  */
 int isValidNumber(char *num)
 {
+	if (*num == '-')
+		num++;
+
 	while (*num)
 	{
 		if (!isdigit(*num))
@@ -28,8 +31,16 @@ int isValidNumber(char *num)
  */
 void _puts(char *str)
 {
-	if (isValidNumber(str) && *str == '0')
-		str++;
+	if (isValidNumber(str))
+	{
+		if (*str == '-')
+		{
+			_putchar('-');
+			str++;
+		}
+		if (*str == '0')
+			str++;
+	}
 
 	while (*str)
 		_putchar(*str++);
@@ -48,10 +59,15 @@ char *mul(char *n1, char *n2)
 	char *product;
 	char d;
 	int n1_len, n2_len, pr_len, carry, sum, j;
+	int n1_sign_offset, n2_sign_offset, sign_offset;
 
-	n1_len = strlen(n1);
-	n2_len = strlen(n2);
-	pr_len = n1_len + n2_len;
+	n1_sign_offset = *n1 == '-' ? 1 : 0;
+	n2_sign_offset = *n2 == '-' ? 1 : 0;
+
+	n1_len = strlen(n1) - n1_sign_offset;
+	n2_len = strlen(n2) - n2_sign_offset;
+	sign_offset = (*n1 == '-') ^ (*n2 == '-');
+	pr_len = n1_len + n2_len + sign_offset;
 	product = malloc(sizeof(char) * pr_len);
 	if (product == NULL)
 		return (NULL);
@@ -64,21 +80,24 @@ char *mul(char *n1, char *n2)
 
 	n2_len--;
 	n1_len--;
-	while (n2_len >= 0)
+	while (n2_len - n2_sign_offset >= 0)
 	{
 		carry = 0;
 		j = n1_len;
 		while (j >= 0)
 		{
-			d = product[j + n2_len + 1] - 48;
-			sum = carry + d + (n1[j] - 48) * (n2[n2_len] - 48);
+			d = product[j + n2_len + sign_offset + 1] - 48;
+			sum = carry + d + (n1[j + n1_sign_offset] - 48) * (n2[n2_len] - 48);
 			carry = sum / 10;
-			product[j + n2_len + 1] = sum % 10 + '0';
+			product[j + n2_len + sign_offset + 1] = sum % 10 + '0';
 			j--;
 		}
-		product[n2_len] = carry + '0';
+		product[n2_len + sign_offset] = carry + '0';
 		n2_len--;
 	}
+
+	if (sign_offset)
+		product[0] = '-';
 
 	return (product);
 }
