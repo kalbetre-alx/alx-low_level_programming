4daa75e1411497c7f620e21041480e009097556a
kalbetre-alx <kalbetre@icloud.com>
2022-06-30 10:00:29 -0700

    use custom strlen

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,23 @@
-#include <string.h>
 #include <stdlib.h>
+
+/**
+ * _strlen - gets the string length
+ * @str: the string
+ *
+ * Return: length of the string or 0 if NULL
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while(*(str + len))
+		len++;
+	
+	return (len);
+}
 
 /**
  * string_nconcat - combines two strings given the limit for the second
@@ -14,9 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1_len, s2_len, i = 0;
 	char *combo;
 
-	s1_len = s1 == NULL ? 0 : strlen(s1);
-	s2_len = s2 == NULL ? 0 : strlen(s2);
-	n = n > s2_len ? s2_len : n;
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+	n = n >= s2_len ? s2_len : n;
 
 	combo = malloc(sizeof(char) * (s1_len + n + 1));
 	if (combo == NULL)
