91d5ecd7b1db5d8adda51b6fe986053a13ac4ab4
Kalkidan Betre <105339878+kalbetre-alx@users.noreply.github.com>
2022-07-01 03:39:32 +0300

    fix tabs

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -13,7 +13,7 @@ int isValidNumber(char *num)
 {
 	while (*num)
 	{
-		if (*num < '0' || *num > '9')
+		if (!isdigit(*num))
 			return (0);
 		num++;
 	}
@@ -40,47 +40,47 @@ void _puts(char *str)
 
 /**
  * mul - multiplies two large numbers
- * @s1: first number
- * @s2: second number
+ * @n1: first number
+ * @n2: second number
  *
- * Return: s1 times s2
+ * Return: n1 times n2
  */
-char *mul(char *n1, char *n2)                                                                             
-{                                                                                                         
-        char *product;                                                                                    
-        char d;                                                                                           
-        int n1_len, n2_len, pr_len, carry, sum, j = 0;                                                    
-                                                                                                          
-        n1_len = strlen(n1);                                                                              
-        n2_len = strlen(n2);                                                                              
-        pr_len = n1_len + n2_len;                                                                         
-        product = malloc(pr_len + 1);                                                                     
-        if (product == NULL)                                                                              
-                return (NULL);                                                                            
-        while (j < pr_len)                                                                                
-        {                                                                                                 
-                *(product + j) = '0';                                                                     
-                j++;                                                                                      
-        }                                                                                                 
-        n2_len--;                                                                                         
-        n1_len--;                                                                                         
-        while (n2_len >= 0)                                                                               
-        {                                                                                                 
-                carry = 0;                                                                                
-                j = n1_len;                                                                               
-                while (j >= 0)                                                                            
-                {                                                                                         
-                        d = product[j + n2_len + 1] - 48;                                                 
-                        sum = carry + d + (n1[j] - 48) * (n2[n2_len] - 48);                               
-                        carry = sum / 10;                                                                 
-                        product[j + n2_len + 1] = sum % 10 + '0';                                         
-                        j--;                                                                              
-                }                                                                                         
-                product[n2_len] = carry + '0';                                                            
-                n2_len--;                                                                                 
-        }                                                                                                 
-        product[pr_len] = '\0';                                                                           
-        return (product);                                                                                 
+char *mul(char *n1, char *n2)
+{
+	char *product;
+	char d;
+	int n1_len, n2_len, pr_len, carry, sum, j = 0;
+
+	n1_len = strlen(n1);
+	n2_len = strlen(n2);
+	pr_len = n1_len + n2_len;
+	product = malloc(pr_len + 1);
+	if (product == NULL)
+		return (NULL);
+	while (j < pr_len)
+	{
+		*(product + j) = '0';
+		j++;
+	}
+	n2_len--;
+	n1_len--;
+	while (n2_len >= 0)
+	{
+		carry = 0;
+		j = n1_len;
+		while (j >= 0)
+		{
+			d = product[j + n2_len + 1] - 48;
+			sum = carry + d + (n1[j] - 48) * (n2[n2_len] - 48);
+			carry = sum / 10;
+			product[j + n2_len + 1] = sum % 10 + '0';
+			j--;
+		}
+		product[n2_len] = carry + '0';
+		n2_len--;
+	}
+	product[pr_len] = '\0';
+	return (product);
 }
 
 /**
@@ -90,29 +90,28 @@ char *mul(char *n1, char *n2)
  *
  * Return: the multiplication of the numbers
  */
-
 int main(int argc, char **argv)
 {
-        char *result;
+	char *result;
 
-        if (argc != 3 || !isValidNumber(argv[1]) || !isValidNumber(argv[2]))
-        {
-                _puts("Error");
-                exit(98);
-        }
-        else
-        {
-                result = mul(argv[1], argv[2]);
-                if (result == NULL)
-                {
-                        _puts("Error");
-                        exit(98);
-                }
-                else
-                {
-                        _puts(result);
-                        free(result);
-                }
-        }
-        return (0);
+	if (argc != 3 || !isValidNumber(argv[1]) || !isValidNumber(argv[2]))
+	{
+		_puts("Error");
+		exit(98);
+	}
+	else
+	{
+		result = mul(argv[1], argv[2]);
+		if (result == NULL)
+		{
+			_puts("Error");
+			exit(98);
+		}
+		else
+		{
+			_puts(result);
+			free(result);
+		}
+	}
+	return (0);
 }
