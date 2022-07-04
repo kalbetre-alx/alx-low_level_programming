63aae7f70871a799e3b01c6c0a3cea6838d2f044
kalbetre-alx <kalbetre@icloud.com>
2022-07-04 13:32:09 -0700

    restructure funtions

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,33 @@
 #include <stdlib.h>
 
 /**
+ * _strdup - duplicate the given string
+ * @str: given string
+ *
+ * Return: duplicate string or NULL
+ */
+char *_strdup(char *str)
+{
+	unsigned int len = 0;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	dup = malloc(sizeof(char) * (len + 1));
+	if (dup == NULL)
+		return (NULL);
+
+	for (; len >= 0; len--)
+		dup[len] = str[len];
+
+	return (dup);
+}
+
+/**
  * new_dog - creates a new dog sructure
  * @name: a cstring name of the dog
  * @age: age of the dog
@@ -14,39 +41,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	unsigned int name_len = 0, owner_len = 0;
 	dog_t *d;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
 	d = malloc(sizeof(d));
 	if (d == NULL)
 		return (NULL);
 
-	for (name_len = 0; name[name_len] != '\0'; name_len++)
-		;
-	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
-		;
-
-	d->name = malloc(sizeof(char) * (name_len + 1));
+	d->name = _strdup(name);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	for (name_len = 0; name[name_len] != '\0'; name_len++)
-		d->name[name_len] = name[name_len];
 
-	d->owner = malloc(sizeof(char) * (owner_len + 1));
+	d->owner = _strdup(owner);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
-	for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
-		d->owner[owner_len] = owner[owner_len];
-
-	d->name[name_len + 1] = '\0';
-	d->owner[owner_len + 1] = '\0';
 	d->age = age;
 	return (d);
 }
