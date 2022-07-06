e48b4d919a9e0f551b343d94cea687ad4a4b6ce5
kalbetre-alx <kalbetre@icloud.com>
2022-07-06 17:15:49 +0000

    add a check for null op

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].f != NULL)
 	{
-		if (*ops[i].op == *s)
+		if (s && *ops[i].op == *s)
 			break;
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -41,4 +41,3 @@ int main(int argc, char **argv)
 	printf("%d\n", fun(a, b));
 	return (0);
 }
-
