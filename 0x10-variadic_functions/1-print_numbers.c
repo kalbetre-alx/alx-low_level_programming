b32c4ba2146e9ab586910d059d115deff1e0bd97
kalbetre-alx <kalbetre@icloud.com>
2022-07-07 19:14:14 +0000

    print new line nomater what

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,16 +15,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ls;
 	unsigned int i;
 
-	if (separator == NULL || n == 0)
-		return;
-
 	va_start(ls, n);
 
-	for (i = 0; i < n; i++)
+	if (separator != NULL)
 	{
-		if (i != 0)
-			printf("%s", separator);
-		printf("%d", va_arg(ls, int));
+		for (i = 0; i < n; i++)
+		{
+			if (i != 0)
+				printf("%s", separator);
+			printf("%d", va_arg(ls, int));
+		}
 	}
 
 	printf("\n");
