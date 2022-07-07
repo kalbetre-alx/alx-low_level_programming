a4b5b22a52e63e145898741536936f37869c55cb
kalbetre-alx <kalbetre@icloud.com>
2022-07-07 19:17:22 +0000

    print numbers with NULL separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,16 +17,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(ls, n);
 
-	if (separator != NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i != 0)
-				printf("%s", separator);
-			printf("%d", va_arg(ls, int));
-		}
+		if (i != 0 && separator != NULL)
+			printf("%s", separator);
+		printf("%d", va_arg(ls, int));
 	}
-
+	
 	printf("\n");
 	va_end(ls);
 }
