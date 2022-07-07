e204491186084163a205219394392431b26190b4
kalbetre-alx <kalbetre@icloud.com>
2022-07-07 19:06:05 +0000

    add function to print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: a separator to be used
+ * @n: the count of numbers passed
+ *
+ * Return: Nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ls;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(ls, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+			printf("%s", separator);
+		printf("%d", va_arg(ls, int));
+	}
+
+	printf("\n");
+	va_end(ls);
+}
