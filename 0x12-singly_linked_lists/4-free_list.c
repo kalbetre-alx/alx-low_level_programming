68968065afd087dcf8dd3ee0204497cb3dce73d8
kalbetre-alx <kalbetre@icloud.com>
2022-07-14 17:01:58 +0000

    add function to free a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - frees the memory allocated for list
+ * @head: pointer to the head of the list
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	if (head->next != NULL)
+		free_list(head->next);
+
+	free(head->str);
+	free(head);
+}
