258dd7acb17e2f41513580bbc5f3cacdc9905dd0
kalbetre-alx <kalbetre@icloud.com>
2022-07-20 21:23:38 +0000

    use bitwise op for binary to unit function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,5 @@
 #include "main.h"
 #include <stddef.h>
-#include <stdio.h>
-#include <string.h>
-
-/**
- * pow_2 - calculates the 2 the power of x
- * @x: power
- *
- * Return: 2 to the power of x
- */
-unsigned int pow_2(int x)
-{
-	unsigned int num = 1;
-
-	while (x--)
-		num *= 2;
-
-	return (num);
-}
 
 /**
  * binary_to_uint - returns uint equivalent of given bin
@@ -28,19 +10,17 @@ unsigned int pow_2(int x)
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int len = 0, pow = 0;
 
 	if (b == NULL)
 		return (0);
 
-	len = strlen(b) - 1;
-	while (len >= 0)
+	while (*b != '\0')
 	{
-		if (b[len] == '1' || b[len] == '0')
+		if (*b == '1' || *b == '0')
 		{
-			num += ((int)(b[len]) - 48) * (int)pow_2(pow);
-			len--;
-			pow++;
+			num <<= 1;
+			num += *b == '1' ? 1 : 0;
+			b++;
 		}
 		else
 			return (0);
