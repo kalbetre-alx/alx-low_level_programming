04705b99d39d9544f7f0035395a0de34eb1a82c6
kalbetre-alx <kalbetre@icloud.com>
2022-07-20 14:23:35 +0000

    add clear bit function

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,120 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * pow_2 - calculates the 2 the power of x
+ * @x: power
+ *
+ * Return: 2 to the power of x
+ */
+unsigned int pow_2(int x)
+{
+	unsigned int num = 1;
+
+	while (x--)
+		num *= 2;
+
+	return (num);
+}
+
+/**
+ * uint_to_binary - converts a number to binary
+ * @n: the unint number
+ *
+ * Return: a string containing th binary digits
+ */
+char *uint_to_binary(unsigned long int n)
+{
+	char *str;
+	int len = 0, i = 0;
+	unsigned int pow;
+
+	if (n == 0 || n == 1)
+	{
+		str = malloc(sizeof(char) * 1 + 1);
+		if (str == NULL)
+			return (NULL);
+		str[0] = n + '0';
+		str[1] = '\0';
+	}
+	else
+	{
+		while (pow_2(len) <= n)
+			len++;
+
+		str = malloc(sizeof(char) * len);
+		if (str == NULL)
+			return (NULL);
+		str[len--] = '\0';
+		while (i <= len)
+		{
+			pow = pow_2(len - i);
+			str[i++] = n >= pow ? '1' : '0';
+			n = n >= pow ? n - pow : n;
+		}
+	}
+
+	return (str);
+}
+
+/**
+ * binary_to_uint - returns uint equivalent of given bin
+ * @b: pointer to a string of 0's and 1's
+ *
+ * Return: converted number or 0 if failed
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int len = 0, pow = 0;
+
+	if (b == NULL)
+		return (0);
+
+	len = strlen(b) - 1;
+	while (len >= 0)
+	{
+		if (b[len] == '1' || b[len] == '0')
+		{
+			num += ((int)(b[len]) - 48) * (int)pow_2(pow);
+			len--;
+			pow++;
+		}
+		else
+			return (0);
+	}
+
+	return (num);
+}
+
+/**
+ * clear_bit - clears the value of a bit at a given index
+ * @n: the number
+ * @index: the zero based index
+ *
+ * Return: 1 if it succeeded or -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	char *bin_str;
+	unsigned int len;
+
+	if (n == NULL)
+		return (-1);
+
+	bin_str = uint_to_binary(*n);
+	if (bin_str == NULL)
+		return (-1);
+
+	len = strlen(bin_str);
+	if (index <= (len - 1))
+	{
+		bin_str[len - index - 1] = '0';
+		*n = binary_to_uint(bin_str);
+	}
+
+	free(bin_str);
+	return (1);
+}
